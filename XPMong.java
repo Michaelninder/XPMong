f20ca66941c7f12a15ace6113c84651321d928d0
Michaelninder <151365993+Michaelninder@users.noreply.github.com>
2024-04-30 18:27:36 +0200

    Update XPMong.java

--- XPMong.java
+++ XPMong.java
@@ -15,6 +15,8 @@ import java.util.*;
 public class XPMong extends JavaPlugin implements Listener {
     private HashMap<Player, Integer> scores = new HashMap<>();
     private int gameTime = 0;
+    private boolean gameRunning = false;
+    private int countdownTime = 15; // Countdown time in seconds
     private Scoreboard scoreboard;
     private Objective objective;
 
@@ -75,6 +77,17 @@ public class XPMong extends JavaPlugin implements Listener {
             spleeverItem = new ItemStack(material);
             player.sendMessage("Spleever item set to " + material.toString());
             return true;
+        } else if (command.getName().equalsIgnoreCase("startgame")) {
+            if (gameRunning) {
+                sender.sendMessage("Game is already running!");
+                return true;
+            }
+            if (Bukkit.getOnlinePlayers().size() < 2) {
+                sender.sendMessage("Not enough players to start the game!");
+                return true;
+            }
+            startCountdown();
+            return true;
         }
         return false;
     }
@@ -82,10 +95,10 @@ public class XPMong extends JavaPlugin implements Listener {
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
+        if (!gameRunning || !player.getWorld().getName().equalsIgnoreCase("Mong")) {
+            return;
+        }
         if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            if (!player.getWorld().getName().equalsIgnoreCase("Mong")) {
-                return;
-            }
             if (player.getInventory().getItemInMainHand().isSimilar(spleeverItem)) {
                 // Implement spleever functionality
                 // Determine block clicked and remove it if necessary
@@ -96,7 +109,10 @@ public class XPMong extends JavaPlugin implements Listener {
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
         Player player = (Player) event.getWhoClicked();
-        if (event.getInventory().getHolder() == player && player.getWorld().getName().equalsIgnoreCase("Mong")) {
+        if (!gameRunning || !player.getWorld().getName().equalsIgnoreCase("Mong")) {
+            return;
+        }
+        if (event.getInventory().getHolder() == player) {
             event.setCancelled(true);
             ItemStack clickedItem = event.getCurrentItem();
             if (clickedItem != null && clickedItem.getType() != Material.AIR) {
@@ -124,6 +140,56 @@ public class XPMong extends JavaPlugin implements Listener {
         }.runTaskTimer(this, 20, 20); // Run every second
     }
 
+    private void startCountdown() {
+        gameRunning = true;
+        new BukkitRunnable() {
+            int timeLeft = countdownTime;
+
+            @Override
+            public void run() {
+                if (timeLeft == 0) {
+                    cancel();
+                    // Start the game
+                    Bukkit.broadcastMessage(ChatColor.GREEN + "Game started!");
+                    // Give players necessary items
+                    for (Player player : Bukkit.getOnlinePlayers()) {
+                        player.getInventory().clear();
+                        player.getInventory().setItem(0, createTeamSelector());
+                        player.getInventory().setItem(4, createInventorySorter());
+                        player.getInventory().setItem(8, createItemSelector());
+                    }
+                } else {
+                    Bukkit.broadcastMessage(ChatColor.YELLOW + "Game starting in " + timeLeft + " seconds...");
+                    timeLeft--;
+                }
+            }
+        }.runTaskTimer(this, 0, 20); // Run every second
+    }
+
+    private ItemStack createTeamSelector() {
+        ItemStack item = new ItemStack(Material.COMPASS);
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(ChatColor.GREEN + "Team Selector");
+        item.setItemMeta(meta);
+        return item;
+    }
+
+    private ItemStack createInventorySorter() {
+        ItemStack item = new ItemStack(Material.COMPASS);
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(ChatColor.GREEN + "Inventory Sorter");
+        item.setItemMeta(meta);
+        return item;
+    }
+
+    private ItemStack createItemSelector() {
+        ItemStack item = new ItemStack(Material.COMPASS);
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(ChatColor.GREEN + "Item Selector");
+        item.setItemMeta(meta);
+        return item;
+    }
+
     private boolean checkWinConditions() {
         // Implement win conditions here
         // Return true if a team has 10 points
@@ -131,7 +197,32 @@ public class XPMong extends JavaPlugin implements Listener {
     }
 
     private void endGame() {
+        gameRunning = false;
         // Implement game end logic here
         // Reset scores, refill platform, etc.
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        if (gameRunning && player.getWorld().getName().equalsIgnoreCase("Mong")) {
+            setSpectator(player);
+        }
+    }
+
+    private void setSpectator(Player player) {
+        player.setGameMode(GameMode.SPECTATOR);
+        player.getInventory().clear();
+        player.getInventory().setItem(4, createSpectatorCompass());
+        player.setInvisible(true);
+        player.setAllowFlight(true);
+    }
+
+    private ItemStack createSpectatorCompass() {
+        ItemStack item = new ItemStack(Material.COMPASS);
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(ChatColor.GREEN + "Spectator Menu");
+        item.setItemMeta(meta);
+        return item;
     }
 }
